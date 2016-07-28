8b9417239325b573fd4201fd0bc1b23bfb8a2592
ntabgoba <ntabgoba@gmail.com>
2016-07-28 19:24:38 +0900

    looking at 2013 dataset

--- mina_niho/nihomatsu.R
+++ mina_niho/nihomatsu.R
@@ -13,6 +13,8 @@ library(Hmisc)
 
 # Loading June 2011 Fukushima Data and selecting Nihomatsu's.
 niho <- read_csv("jun_2011_fukushima.csv")
+niho2013 <- read_csv("nov_2013_fukushima.csv")
+View(niho2013)
 names(niho)
 dim(niho)
 head(niho)
@@ -22,14 +24,23 @@ names(niho) <- c("gridcode","pref","city","gridCenterNorthlat","gridCenterEastln
                  "gridCenterEastlngDec","daichi_distance","no_samples","AvgAirDoseRate",
                        "NE_nLat","NE_eLong","NW_nLat","NW_eLong",
                        "SW_nLat","SW_eLong","SE_nLat","SE_eLong")
+names(niho2013) <- c("gridcode","pref","city","gridCenterNorthlat","gridCenterEastlng","gridCenterNorthlatDec",
+                     "gridCenterEastlngDec","daichi_distance","no_samples","AvgAirDoseRate",
+                     "NE_nLat","NE_eLong","NW_nLat","NW_eLong",
+                     "SW_nLat","SW_eLong","SE_nLat","SE_eLong")
 View(niho)
 summary(niho$AvgAirDoseRate)
+summary(niho2013$AvgAirDoseRate)
 #Strip Nihommatsu city,
 niho$city[niho$city == "Nihommatsu city"] <- "nihommatsu"
+niho2013$city[niho2013$city == "Nihommatsu city"] <- "nihommatsu"
 
 #filter nihomatsu dataset
 nihom <- subset(niho, city == "nihommatsu")
+nihom2013 <- subset(niho2013, city == "nihommatsu")
 dim(nihom)
+dim(nihom2013)
+View(nihom2013)
 class(nihom)
 # Create air dose quantiles that are plot-able,6 categorical variables.
 niho_q <- nihom %>%
@@ -37,6 +48,15 @@ niho_q <- nihom %>%
 View(niho_q)
 niho_q <- na.omit(niho_q)
 write_csv(niho_q, path = "niho_q.csv")
+
+nihom2013_q <- nihom2013 %>%
+        mutate(dose_quants = cut2(nihom2013$AvgAirDoseRate,cuts=c(0.1,0.5,1.0,1.5,2.0,2.5,3.0),levels.mean=TRUE))
+summary(nihom2013$AvgAirDoseRate)
+summary(niho_q$AvgAirDoseRate)
+## Visible reduction of Average Air Dose Distribution by half  in Nihomatsu, 
+#Trouble is knowing the distribution of causes of this reduction?
+plot(nihom2013$AvgAirDoseRate,niho_q$AvgAirDoseRate)
+
 #Color function
 iro <- colorFactor(
         palette = "YlOrRd",
@@ -109,8 +129,8 @@ View(mina_q)
 mina_q <- na.omit(mina_q)
 write_csv(mina_q, path = "mina_q.csv")
 #Color function
-iro <- colorFactor(
-        palette = "BuPu",
+mina_iro <- colorFactor(
+        palette = "Set1",
         domain = mina_q$mina_quants
 )
 # Link of Daichi
@@ -126,11 +146,24 @@ max(mina_q$SE_eLong)
 mina_plot <- leaflet() %>%
         addTiles()%>%
         addRectangles(data = mina_q,lng1 = ~SW_eLong, lat1 = ~SW_nLat,lng2 = ~NE_eLong, lat2 = ~NE_nLat,
-                      color = ~iro(mina_q$mina_quants))%>%
-        addLegend("bottomright", pal = iro, values = mina_q$mina_quants,
+                      color = ~mina_iro(mina_q$mina_quants))%>%
+        addLegend("bottomright", pal = mina_iro, values = mina_q$mina_quants,
                   title = "AvgAirDoseRates",
                   labFormat = labelFormat(prefix = "µSv/h "),
                   opacity = 1)%>%
         addPopups(lat = 37.4211, lng = 141.0328,popup = fukulink,
                   options = popupOptions(closeButton = TRUE)) 
 mina_plot
+
+#plot Minamisoma geo locations to Average Air Dose
+ggplot(mina_q, aes(daichi_distance,AvgAirDoseRate)) +
+        geom_point() +
+        geom_smooth(se = FALSE)+
+        ggtitle("AvgAirDose against Distance to Daichi Plant")
+
+
+# Repeat above visualization from a different view 
+ggplot(data = mina_q) +
+        geom_bar(mapping = aes(x = daichi_distance, fill = mina_quants), width = 1)+
+        ggtitle("AvgAirDose Measured Counts against Daichi Distance")
+
