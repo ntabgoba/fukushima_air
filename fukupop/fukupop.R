f99aa4e4916a7bdd5a54bd9ee09787066f39fa25
ntabgoba <ntabgoba@gmail.com>
2016-09-18 17:35:28 +0900

    re-naming census codes

--- fukupop/fukupop.R
+++ fukupop/fukupop.R
@@ -1,6 +1,10 @@
-setwd("/Users/ntabgoba/Desktop/fukupop/")
+setwd("/Users/ntabgoba/Desktop/fukushima_air/fukupop")
 list.files()
 fukpop <- read.table("tblT000609H56400.txt",sep = ",")
 fukpop <- fukpop[-2,]
 View(fukpop)
 summary(fukpop)
+fukpop$V2[fukpop$V2 == "T000609001"] <- "totalpop"
+fukupop <- setNames(fukpop,c("gridcode","totalpop","male","female","household"))
+View(fukupop)
+fukupop <- fukupop[-1,]
