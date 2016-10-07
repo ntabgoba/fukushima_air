f7bfb665a4344025a3b7ad13f2b4e2a7f7111e4b
ntabgoba <ntabgoba@gmail.com>
2016-10-07 17:31:21 +0900

    re-calculate air avg against 500m sq. pop

--- mina_niho/trials.R
+++ mina_niho/trials.R
@@ -1,4 +1,6 @@
 # learn merge
 data1 <- data.frame(a = c("ji","hi","we"), b = c("jo","ho","wo"))
-data2 <- data.frame(c = c(2,3,4), b = c())
+data2 <- data.frame(c = c(2112,3112,4112), d = c(5312,6312,7312))
+data3 <- merge(data1,data2)
+data3$c <- gsub('.{1}$', '', data3$c)
                     
