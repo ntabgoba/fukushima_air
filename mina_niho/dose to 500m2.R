5cd50f3df0ce5be977157b7232dd6472f89eec74
ntabgoba <ntabgoba@gmail.com>
2016-10-19 03:35:16 +0900

    popAir map

--- mina_niho/dose to 500m2.R
+++ mina_niho/dose to 500m2.R
@@ -2,6 +2,7 @@
 # Dose is 100m2 i.e third order mesh/10x10, Popn is 500m2 i.e third order mesh/4(SW,SE,NW,NE)
 fuk2013dose <- read.csv("fuk2013dose.csv")
 fuk_pll <- read.csv("fuk_pop.csv")
+fuk_pll$gridcode <- as.character(fuk_pll$gridcode)
 #length of each dataset
 length(unique(fuk_pll$gridcode)); length(unique(fuk2013dose$gridcode))
 
@@ -35,9 +36,14 @@ for (i in 1:length(gridelt1)){
 # create the new var
 fuk2013dose$gridcode <- mycode
 fuk2013dose$gridcode <- gsub("_","",fuk2013dose$gridcode)
-View(fuk2013dose)
+
+fukdose.unique <- fuk2013dose[!duplicated(fuk2013dose[, c("gridcode")]), ]
+
+popAir_merge <- merge(x = fukdose.unique, y = fuk_pll, by.x = "gridcode", by.y = "gridcode")
+View(popAir_merge)
+
 # Check for compatibility with the Fuk Population data
-length(unique(fuk2013dose$gridcode))  #16233
+length(unique(fuk2013dose$gridcode))  #16233  
 length(unique(fuk_pop$gridcode)) #10831
 
 #pick unique grides available to both only.
@@ -47,11 +53,15 @@ unique.air_gride <- unique(fuk2013dose$gridcode)
 length(intersect(fuk_pop$gridcode,fuk2013dose$gridcode)) #6232
 length(setdiff(fuk_pop$gridcode,fuk2013dose$gridcode)) #4599
 length(setdiff(as.character(unique.pop_gride),unique.air_gride)) # 4599
-length(setequal(as.character(unique.pop_gride),unique.air_gride)) #1
+length(setequal(as.character(unique.pop_gride),unique.air_gride)) #1  #6232(merged dataset length)
 
 gride.intersect <- intersect(fuk_pop$gridcode,fuk2013dose$gridcode)
 class(gride.intersect); length(gride.intersect)
 gride.intersect[1]
+
+
+
+
 # gride_matcher function
 meshes <- list()
 gride_matcher <- function(mesh1,mesh2){
@@ -73,8 +83,40 @@ library(dplyr)
 unique.grides.pop <- filter(fuk_pll, gridcode == unique.pop_gride)
 View(unique.grides.pop)
 unique.grides.air <- mutate(fuk2013dose, unique.air_gride)
-View(unique.grides.air)
-unique.air_gride
 
-unique.pop_gride
+# relationship between 
+library(Hmisc)
+library(leaflet)
+library(readr)
+library(dplyr)
+library(RColorBrewer)
+library(ggplot2)
 
+popAir_merged <- popAir_merge %>%
+        mutate(pop_quants = (cut2(popAir_merge$totalpop,cuts=c(10,100,500,1000,1500,2000,2500,3000),levels.mean=TRUE,digits=0)))
+
+
+#plots
+iro <- colorFactor(
+        palette = "YlOrRd",
+        domain = popAir_merged$pop_quants
+)
+
+fukulink <- paste(sep = "<br/>",
+                  "<br><a href='http://www.tepco.co.jp
+                  /en/decommision/index-e.html'>Fukushima Daichi</a></b>",
+                  "Source of radiations")
+
+
+popAir_plot <- leaflet() %>%
+        addTiles()%>%
+        addRectangles(data = popAir_merged,lng1 = ~SW_eLong, lat1 = ~SW_nLat,
+                      lng2 = ~NE_eLong, lat2 = ~NE_nLat,
+                      color = ~iro(popAir_merged$pop_quants))%>%
+        addLegend("bottomright", pal = iro, values = popAir_merged$pop_quants,
+                  title = "Population Dist",
+                  labFormat = labelFormat(prefix = "no. of pple"),
+                  opacity = 1)%>%
+        addPopups(lat = 37.4211, lng = 141.0328,popup = fukulink,
+                  options = popupOptions(closeButton = TRUE)) 
+popAir_plot
